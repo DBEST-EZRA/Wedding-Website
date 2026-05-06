8ac7ae152ef42de9cbdec643741d89a7ab4a453c
DBEST-EZRA <ezradbest101@gmail.com>
2026-05-06 19:03:17 +0300

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Hello, Mosioma's Wedding</div>
+      <div className="container text-center mt-5">
+        <h1 className="text-primary">Mosioma's Wedding Invitation</h1>
+        <button className="btn btn-success mt-3">RSVP</button>
+      </div>
     </>
   );
 }
